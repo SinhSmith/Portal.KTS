5055d200ce86852419d87f4b4143aa439efb7438
vspro.91@gmail.com <vspro.91@gmail.com>
2016-09-08 21:52:37 +0700

    init project

---
+++ Site.OnlineStore/Controllers/BaseController.cs
@@ -0,0 +1,42 @@
+﻿using Portal.Service.Implements;
+using Portal.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Site.OnlineStore.Controllers
+{
+    public class BaseController : Controller
+    {
+        #region Properties
+
+        #endregion
+        
+        #region Constructures
+
+        public BaseController()
+        {
+        }
+
+        #endregion
+
+        #region Private functions
+
+        #endregion
+
+        #region Release resources
+
+        /// <summary>
+        /// Dispose database connection
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+        #endregion
+    }
+}
---
+++ Site.OnlineStore/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using Portal.Service.Implements;
+using Portal.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Site.OnlineStore.Controllers
+{
+    public class HomeController : BaseController
+    {
+        #region Properties
+
+        IBannerService _bannerService = new BannerService();
+        ICMSNewsService _cmsNewsService = new CMSNewsService();
+
+        #endregion
+
+        #region Constructures
+
+        public HomeController()
+        {
+            _bannerService = new BannerService();
+            _cmsNewsService = new CMSNewsService();
+        }
+
+        #endregion
+
+        #region Actions
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult _HeaderPartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult BannerPartial()
+        {
+            return PartialView(_bannerService.GetBanners1ForHomePage());
+        }
+
+        public ActionResult BlogPartial()
+        {
+            return PartialView(_cmsNewsService.GetCMSNewsForHomePage());
+        }
+
+        #endregion
+
+        #region Release resources
+
+        /// <summary>
+        /// Dispose database connection
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+        #endregion
+    }
+}
